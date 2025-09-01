ecf034030da06c660f16bd3d5c9d29377df1e78d
Reece Balfour <reece@RBMP.local>
2025-09-01 15:17:55 +0100

    db: update json seed data and table schema properties to allow for optional ids to be passed when seeding. add guard to seedDBJSON.ts to rollback on error

--- db/seeds/seedDBJSON.ts
+++ db/seeds/seedDBJSON.ts
@@ -2,6 +2,7 @@ import collectionsJSON from "./data/collections.json";
 import itemsJSON from "./data/items.json";
 import itemDataJSON from "./data/item_data.json";
 import db from "../connection";
+import type { PoolClient } from 'pg';
 import { CollectionInput, ItemInput, SingleItemDataInput, ItemDataType } from "chartsy-types";
 const collections = collectionsJSON as CollectionInput[];
 const items = itemsJSON as ItemInput[];
@@ -9,57 +10,71 @@ const itemData = itemDataJSON as SingleItemDataInput[];
 
 export const runSeed = async (collections: CollectionInput[], items: ItemInput[], itemData: SingleItemDataInput[]) => {
   console.log("Seeding...");
-  await db.query(`TRUNCATE TABLE collections RESTART IDENTITY CASCADE;`);
-  await db.query(`TRUNCATE TABLE items RESTART IDENTITY CASCADE;`);
-  await db.query(`TRUNCATE TABLE item_data RESTART IDENTITY CASCADE;`);
-  await insertCollections(collections);
-  await insertItems(items);
-  await insertItemData(itemData);
+  const client: PoolClient = await db.connect();
+  try {
+  // using single client instead of full pool to ensure no conflict of connections causing incorrect ids
+  // client query begin, commit and rollback ensure no half seeded data if error
+  await client.query("BEGIN");
+  await client.query(`TRUNCATE TABLE collections RESTART IDENTITY CASCADE;`);
+  await insertCollections(client, collections);
+  await insertItems(client, items);
+  await insertItemData(client, itemData);
+  await client.query("COMMIT");
   console.log("Seeding complete.");
+  } catch(err) {
+    await client.query("ROLLBACK");
+    console.error("Seeding failed, rolling back: ", err);
+    throw err;
+  } finally {
+    client.release();
+  }
 };
 
-const insertCollections = async (collections: CollectionInput[]) => {
+const insertCollections = async (client: PoolClient, collections: CollectionInput[]) => {
   for (const collection of collections) {
     try {
-      await db.query(
+      await client.query(
         `INSERT INTO collections (collection_name, icon)
          VALUES ($1, $2);`,
         [collection.collection_name, collection.icon]
       );
     } catch (err: any) {
       console.error("Error seeding collection. Reason:", err);
+      throw err;
     }
   }
 };
 
-const insertItems = async (items: ItemInput[]) => {
+const insertItems = async (client: PoolClient, items: ItemInput[]) => {
   for (const item of items) {
     try {
-      await db.query(
+      await client.query(
         `INSERT INTO items (collection_id, item_name, icon)
          VALUES ($1, $2, $3);`,
         [item.collection_id, item.item_name, item.icon]
       );
     } catch (err: any) {
       console.error("Error seeding item. Reason:", err);
+      throw err;
     }
   }
 };
 
-const insertItemData = async (itemData: SingleItemDataInput[]) => {
+const insertItemData = async (client: PoolClient, itemData: SingleItemDataInput[]) => {
   for (const data of itemData) {
     try {
       if (!["text", "image", "file"].includes(data.data_type)) {
         console.error(`Invalid data_type for item data: ${data.data_type}`);
         continue;
       }
-      await db.query(
+      await client.query(
         `INSERT INTO item_data (item_id, data_type, data_body)
          VALUES ($1, $2, $3);`,
         [data.item_id, data.data_type as ItemDataType, data.data_body]
       );
     } catch (err: any) {
       console.error("Error seeding item data. Reason:", err);
+      throw err;
     }
   }
 };
