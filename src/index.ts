67d5e09f97f92116fb572b06bcd0ca4af54ba9b1
Reece Balfour <reece@RBMP.local>
2025-11-13 15:42:29 +0000

    FIX: index.ts - ensure correct aliasing for dev vs prod (local changes were not reflected in dev environment as was pointing to dist)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import "module-alias/register";
-import { app } from "@/app";
+const isProd = process.env.NODE_ENV === "production";
+// In production, set up module-alias so @ points at dist/src
+if (isProd) {
+  require("module-alias/register");
+}
+// In dev (ts-node), tsconfig-paths will resolve "@/app" to src/app.ts
+// In prod (node dist/index.js), module-alias will resolve "@/app" to dist/src/app.js
+const { app } = require("@/app");
+
 
 const PORT = process.env.PORT || 4000;
 
